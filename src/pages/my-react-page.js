ac9d5c613f9f3d326eb9c9b7b595500000f3f385
Machao <mafye@126.com>
2024-06-14 16:27:22 +0800

    react page change

--- src/pages/my-react-page.js
+++ src/pages/my-react-page.js
@@ -6,7 +6,6 @@ export default function MyReactPage() {
     <Layout>
       <h1>My React page</h1>
       <p>This is a React page</p>
-      <iframe src="https://www.geogebra.org/calculator/rumamjty?embed" width="800" height="600" allowfullscreen style="border: 1px solid #e4e4e4;border-radius: 4px;" frameborder="0"></iframe>
     </Layout>
   );
 }
