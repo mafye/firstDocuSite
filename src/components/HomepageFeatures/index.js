7ad97364f611e6c76e0c1dfd052716fa35f01ebf
mafye <mafye@126.com>
2025-04-01 09:01:41 +0800

    修改图片链接问题

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+// 使用图片而不是SVG
 const FeatureList = [
   {
     title: '专业教学团队',
-    Svg: require('@site/static/img/undraw_teaching.svg').default,
+    imageSrc: '/img/feature1.png', // 替换为您的图片路径
     description: (
       <>
         葫芦学堂拥有一支经验丰富的教师团队，他们不仅具备扎实的专业知识，
@@ -15,7 +16,7 @@ const FeatureList = [
   },
   {
     title: '科学的课程体系',
-    Svg: require('@site/static/img/undraw_education.svg').default,
+    imageSrc: '/img/feature2.png', // 替换为您的图片路径
     description: (
       <>
         我们的课程体系经过精心设计，从基础到进阶，层层递进，
@@ -25,7 +26,7 @@ const FeatureList = [
   },
   {
     title: '互动学习体验',
-    Svg: require('@site/static/img/undraw_interaction.svg').default,
+    imageSrc: '/img/feature3.png', // 替换为您的图片路径
     description: (
       <>
         葫芦学堂注重互动教学，通过丰富多样的教学活动和实践项目，
@@ -35,12 +36,12 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({imageSrc, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className={styles.featureCard}>
         <div className="text--center">
-          <Svg className={styles.featureSvg} role="img" />
+          <img src={imageSrc} className={styles.featureImage} alt={title} />
         </div>
         <div className="text--center padding-horiz--md">
           <h3>{title}</h3>
